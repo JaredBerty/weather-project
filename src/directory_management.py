436f2d5855e3c3f9889f087b64e7fbe5c2cb3bfc
JaredBerty <jaredberty1@gmail.com>
2024-05-02 19:39:55 -0600

    now manages directories

--- src/directory_management.py
+++ src/directory_management.py
@@ -9,9 +9,7 @@ class DirectoryManager:
         os.chdir(config.JSON_DATA_DIR)
         print("changed directory to JSON data folder.")
 
-
     @staticmethod
     def root():
         os.chdir(config.ROOT_DIR)
         print("Changed directory to parent folder")
-
